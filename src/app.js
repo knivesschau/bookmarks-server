d9b92a68451b2173f88681f697c6d593ccb5da25
Catrina Fuentes <cat.fuentes93@gmail.com>
2020-05-03 23:03:31 -0700

    setting up modules

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(helmet());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!')
+    res.send('Welcome to the Bookmarks API Client!')
 });
 
 app.use(function errorHandler(error, req, res, next) {
