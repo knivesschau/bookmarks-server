febfbef75cba5c471fe2997204efbccfdfd0d866
Catrina Fuentes <cat.fuentes93@gmail.com>
2020-05-03 23:23:57 -0700

    set up router stubs

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -1,2 +1,24 @@
 const express = require('express'); 
 const logger = require('../logger');
+const {v4:uuid} = require('uuid');
+
+const bookmarksRouter = express.Router();
+const bodyParser = express.json();
+
+bookmarksRouter
+    .route('/bookmarks')
+    .get((req,res) => {
+
+    })
+    .post((req, res) => {
+
+    });
+
+bookmarksRouter 
+    .route('/bookmarks/:id')
+    .get((req,res) => {
+
+    })
+    .delete((req,res) => {
+
+    });
